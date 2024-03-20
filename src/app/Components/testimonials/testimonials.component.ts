207b32411d5a446e40dfcbb1c43b03af3c03b634
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2024-03-20 09:25:57 +0000

    Complete landing page

--- src/app/Components/testimonials/testimonials.component.ts
+++ src/app/Components/testimonials/testimonials.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-testimonials',
   standalone: true,
   imports: [],
   templateUrl: './testimonials.component.html',
-  styleUrl: './testimonials.component.scss'
+  styleUrl: './testimonials.component.scss',
+  
 })
 export class TestimonialsComponent {
 
